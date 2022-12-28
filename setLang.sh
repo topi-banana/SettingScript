12196dca79f99635cb749625bf0ad04a494b2097
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-12-28 20:55:22 +0900

    Update setLang.sh

--- setLang.sh
+++ setLang.sh
@@ -1,3 +1,3 @@
-sudo apt install language-pack-ja-base language-pack-ja ibus-mozc -y
+sudo apt install -y language-pack-ja-base language-pack-ja ibus-mozc
 sudo localectl set-locale LANG=ja_JP.UTF-8 LANGUAGE="ja_JP:ja"
 source /etc/default/locale
