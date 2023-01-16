d97e7acede5ff312a38dcbf38ea88d350e6bb20e
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-17 02:08:53 +0900

    Update and rename php-apache.sh to php-apache-mysql.sh

--- source/php-apache-mysql.sh
+++ source/php-apache-mysql.sh
@@ -1,3 +1,6 @@
 sudo apt install apache2
+
+sudo apt install mysql-server
+
 sudo apt install php libapache2-mod-php
 sudo apt install php-fpm php-common php-mbstring php-xmlrpc php-gd php-xml php-mysql php-cli php-zip php-curl php-imagick
