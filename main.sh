75f1a247cdd75b540c434cbfd3b852504c1889d5
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-17 04:26:12 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,3 +2,5 @@ curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/
 curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/netplan.sh | bash
 curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/php-apache-mysql.sh | bash
 curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/mecab.sh | bash
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/addWebRipo.sh | bash
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/cron.sh | bash
