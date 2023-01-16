02a860196cd6afaaeae2bcbf4a25ada33afe03c9
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-17 02:13:06 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,4 @@
-
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/language.sh | bash
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/netplan.sh | bash
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/php-apache-mysql.sh | bash
+curl https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/mecab.sh | bash
