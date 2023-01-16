fc181a630e777faff3d53081126825c5242d7695
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-17 02:06:39 +0900

    Create php-apache.sh

---
+++ source/php-apache.sh
@@ -0,0 +1,3 @@
+sudo apt install apache2
+sudo apt install php libapache2-mod-php
+sudo apt install php-fpm php-common php-mbstring php-xmlrpc php-gd php-xml php-mysql php-cli php-zip php-curl php-imagick
