bed7e0eca0a0a99b050964bf025cd0ca7a12ef18
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-17 11:38:30 +0900

    Update php-apache-mysql.sh

--- source/php-apache-mysql.sh
+++ source/php-apache-mysql.sh
@@ -2,5 +2,5 @@ sudo apt install apache2
 
 sudo apt install mysql-server
 
-sudo apt install php libapache2-mod-php
+sudo apt install php libapache2-mod-php php-cli
 sudo apt install php-fpm php-common php-mbstring php-xmlrpc php-gd php-xml php-mysql php-cli php-zip php-curl php-imagick
