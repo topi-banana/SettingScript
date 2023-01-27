0c40ea694237c2b224dc1ebe9e8d0f773acc150a
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-27 22:50:24 +0900

    Update k8s.sh

--- source/k8s.sh
+++ source/k8s.sh
@@ -1,8 +1,8 @@
 sudo snap install microk8s --classic
 
+sudo snap alias microk8s.kubectl kubectl
+
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
 
 sudo reboot
-
-sudo snap alias microk8s.kubectl kubectl
