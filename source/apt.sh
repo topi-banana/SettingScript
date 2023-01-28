03ca5eba871542882ecf61187e66df86adfd6935
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-28 17:57:10 +0900

    Update apt.sh

--- source/apt.sh
+++ source/apt.sh
@@ -1,6 +1,6 @@
 sudo apt update
-sudo apt full-upgrade
-sudo apt install neofetch sl
-sudo apt autoremove --purge
-sudo apt clean
+sudo apt full-upgrade -y
+sudo apt install -y neofetch sl
+sudo apt autoremove --purge -y
+sudo apt clean -y
 sudo reboot
