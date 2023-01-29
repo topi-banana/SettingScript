79deb1e243db687b7905a23e6174ae2fc89b72cb
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-01-29 16:31:29 +0900

    Update hosts.sh

--- source/hosts.sh
+++ source/hosts.sh
@@ -1,4 +1,7 @@
-echo "192.168.2.24 cfsz5
+echo "
+# servers
+192.168.2.24 cfsz5
 192.168.2.25 cfsx3
 192.168.2.26 flex10
-192.168.2.27 g570" | sudo tee /etc/hosts
+192.168.2.27 g570
+" | sudo tee -a /etc/hosts
