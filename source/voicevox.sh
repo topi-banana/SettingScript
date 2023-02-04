7d38b65d3a6e458497573158c286ad82196ad797
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-02-04 17:22:12 +0900

    Update voicevox.sh

--- source/voicevox.sh
+++ source/voicevox.sh
@@ -1,5 +1,8 @@
-wget https://raw.githubusercontent.com/apple-pi-yhhhh/SettingScript/main/source/voicevox_cpu.tar.xz
-tar -Jxvf voicevox_cpu.tar.xz
-rm voicevox_cpu.tar.xz
+apt install -y wget p7zip-full libsndfile1
 
-sudo apt install libsndfile1
+wget -O voicevox_engine.7z https://github.com/VOICEVOX/voicevox_engine/releases/download/0.13.3/linux-cpu.7z.001
+7z x voicevox_engine.7z
+rm voicevox_engine.7z
+mv linux-cpu .voicevox
+
+chmod 755 .voicevox/run
