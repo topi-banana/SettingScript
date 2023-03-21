7c04e81644def93043fb002de04743fefdbd0576
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-03-22 08:31:56 +0900

    Create docker.sh

---
+++ source/docker.sh
@@ -0,0 +1,24 @@
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo gpasswd -a $USER docker
+
+
+DOCKER_SERVICE_CONFIG_FILE=/lib/systemd/system/docker.service
+
+echo $(sed '/ExecStart/s/$/ -H tcp:\/\/0.0.0.0:2375/g' $DOCKER_SERVICE_CONFIG_FILE | sudo tee $DOCKER_SERVICE_CONFIG_FILE) | grep ExecStart
