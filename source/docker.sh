39148b60873523e5a15dfdfc32247466946428c1
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2023-03-22 15:09:10 +0900

    Update docker.sh

--- source/docker.sh
+++ source/docker.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -14,7 +14,7 @@ echo \
 
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo gpasswd -a $USER docker
 
